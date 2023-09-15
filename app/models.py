f53bb40ead01533cd6d61b8d7cd936cff52a55ed
ax34hk <abuabdou.alaa@gmail.com>
2023-09-15 14:10:10 +0300

    Commit2 15/9/2023

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+import uuid
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
     name_ar = models.CharField(max_length=50)
+    is_movie = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -14,7 +15,9 @@ class Category(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    title = models.CharField(max_length=128)
+    title_ar = models.CharField(max_length=128, null=True, blank=True)
     description = models.CharField(max_length=250)
     year = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -28,4 +31,28 @@ class Movie(models.Model):
         return self.title
     
 
+class Serie(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=250)
+    year = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_tvshow = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+    
+class Episode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=128)
+    number = models.CharField(max_length=8)
+    serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
+    link1 = models.CharField(max_length=250, blank=True, null=True)
+    link2 = models.CharField(max_length=250, blank=True, null=True)
+    link3 = models.CharField(max_length=250, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,20 @@ router.register('MovieVSet', MovieViewset)
 app_name = 'app'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('test/', test, name='test'),
-    path('movie/<int:pk>/', movie, name='movie'),
-    path('submovies/<slug:slug_category>/', submovies, name='submovies'),
     path('api/', include(router.urls)),
+    path('', index, name='index'),
+    path('category/<slug:slug_category>/', category, name='category'),
+    path('search/', search, name='search'),
+    path('movies/', movies, name='movies'),
+    path('series/', series, name='series'),
+    path('tvshows/', tvshows, name='tvshows'),
+    path('latestmovies/', latestmovies, name='latestmovies'),
+    path('latestepisodes/', latestepisodes, name='latestepisodes'),
+    path('movie/<uuid:pk>/', movie, name='movie'),
+    path('serie/<uuid:pk>/', serie, name='serie'),
+    path('watch/<uuid:pk>/', watch, name='watch'),
+    path('watchepisode/<uuid:pk>/', watchepisode, name='watchepisode'),
+    path('playvideo/', playvideo, name='playvideo'),
+
+
 ]
